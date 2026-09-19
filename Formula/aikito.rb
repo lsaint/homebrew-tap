42f9656f2b1b3f001653b5ab48deb840a6b4f0bf
Ethan <ls4int@gmail.com>
2026-09-19 16:24:00 +0800

    aikito 1.44.0

--- Formula/aikito.rb
+++ Formula/aikito.rb
@@ -3,8 +3,8 @@ class Aikito < Formula
 
   desc "Durable workspace for governing context across AI agents"
   homepage "https://github.com/lsaint/aikito"
-  url "https://files.pythonhosted.org/packages/59/26/1bfba254b82f147e48effc67862f4faf4acccee38e28c85b719a406424fa/aikito-1.43.0.tar.gz"
-  sha256 "1b8ed0028c0654f03d0fc98bf03ce0043a16b6c069c970ac89b872911828c0be"
+  url "https://files.pythonhosted.org/packages/24/8e/ff5728f9dad893b86ce2bc7733586614f6dceec9a8c8bacc0cc7ca3ef50b/aikito-1.44.0.tar.gz"
+  sha256 "21ba0caecd6fbe8cf55c1108afd7fd70426bb67dd22ea7b087a3856a0332000b"
   license "MIT"
 
   depends_on "git"
